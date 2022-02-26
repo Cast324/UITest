908a2f4366a19fc54c8d5eb62d1ffba44818b221
Michael Blades <michaelblades12@gmail.com>
2022-02-25 19:06:01 -0600

    Initial Commit

---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Michael Blades on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
