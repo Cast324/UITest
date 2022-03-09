fb35471bb718200abd956c591b00e79148841b01
Michael Blades <michaelblades12@gmail.com>
2022-03-09 15:56:46 -0600

    Basic Bullseye UI

--- UITest/ContentView.swift
+++ UITest/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .kerning(2.0)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
             .padding()
+            Button {
+                self.alertIsVisible = true
+            } label: {
+                Text("HIT ME")
+            }
+            .alert("Hello there!", isPresented: $alertIsVisible) {
+              Button("Awesome!") { }
+            } message: {
+                Text("Slider Value is \(Int(sliderValue.rounded()))")
+            }
+
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+.previewInterfaceOrientation(.landscapeRight)
     }
 }
